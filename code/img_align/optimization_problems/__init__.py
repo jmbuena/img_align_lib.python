6d608aaa880ff405804dc9fb16bd0834f7ac1511
José M. Buenaposada <josemiguel.buenaposada@urjc.es>
2017-08-23 15:47:43 -0400

    Remove the old optimization_problems directory (now we have cost_functions).

--- code/img_align/optimization_problems/__init__.py
+++
@@ -1,7 +0,0 @@
-
-__all__ = {'optimization_problem', 'inverse_compositional_problem', 'homography_2d_inv_comp_problem'}
-
-# # Optimization problems
-# from .optimization_problem import OptimizationProblem
-# from .inverse_compositional_problem import InverseCompositionalProblem
-# from .homography_2d_inv_comp_problem import Homography2DInvCompProblem
