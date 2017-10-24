cc8c9fb7848420352ef0803eec622d1f05940404
José M. Buenaposada <josemiguel.buenaposada@urjc.es>
2017-10-24 16:21:17 -0400

    Fixed inv_comp.py execution.

--- code/img_align/__init__.py
+++ code/img_align/__init__.py
@@ -1,12 +1,11 @@
-__all__ = {'utils', 'object_models', 'motion_models', 'cost_functions', 'optimizers', 'tracker', 'test'}
+#__all__ = {'utils', 'object_models', 'motion_models', 'cost_functions', 'optimizers', 'tracker', 'test'}
 
-# from .utils import *
-# from .tracker import Tracker
-#from . import object_models
-#from . import motion_models
-#from . import cost_functions
-#from . import optimizers
-#from . import test
+from .utils import *
+from . import object_models
+from . import motion_models
+from . import cost_functions
+from . import optimizers
+from . import test
 
 
 
